875aa9f92307e3b50ce5c2748b8c066a75945b7d
Adrian Deutsch <adriandeutsch@Adrians-MacBook-Pro.local>
2015-07-15 15:07:43 +1000

    unit5update

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,8 +35,10 @@ function getComputerMove(move) {
     return move || randomPlay();
 }
 
+
+
 function getWinner(playerMove,computerMove) {
-    var winner;
+var winner;   
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
@@ -73,12 +75,33 @@ function getWinner(playerMove,computerMove) {
 return winner;
 }
 
+
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    var playerMove = getPlayerMove;
+    var computerMove = getComputerMove;
+    var winner = getWinner;
+
+
+     while (playerWins < 5 || computerWins < 5) {
+     if (winner === 'player'){
+     playerWins += 1;
+     }
+    else { 
+    computerWins += 1;
+ }
+
+console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+ 
+ }
+  
+  return [playerWins, computerWins];
 }
+
+
 
